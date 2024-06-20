224bddba52f47e64f830651ba374fc9cee5676df
cdp57 <162734441+cdp57@users.noreply.github.com>
2024-06-19 20:36:01 -0400

    Update FrameNetChat.py

--- FrameNetRAG/FrameNetChat.py
+++ FrameNetRAG/FrameNetChat.py
@@ -1,7 +1,11 @@
 from langflow.load import run_flow_from_json
 
+#allows for command line user input
 question = input("Enter your question: ")
 
+#generates a response using the Langflow model and the user input
 result = run_flow_from_json(flow="FrameNetChatFinal.json",
                             input_value=question)
+
+#outputs the response
 print(result[0].outputs[0].results)
